a35ea86cd861467a76f8d7e1cc8ddf388fede7d6
Takudzwa Chidarikire <briansaved@gmail.com>
2020-06-19 23:54:25 +1000

    Date adjustments

--- src/client/js/days.js
+++ src/client/js/days.js
@@ -1,7 +1,7 @@
 let days = (date) => {
   let d = new Date();
   let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
-  let day = String(d.getDate()).padStart(2, "0"); //Creating to digit date format
+  let day = String(d.getDate()).padStart(2, "0"); //Creating two digit date format
   let month = String(d.getMonth() + 1).padStart(2, "0"); //Month is 0 based
   let year = d.getFullYear();
 
@@ -10,7 +10,7 @@ let days = (date) => {
   //   console.log(`dep date is ${date} and today is ${d}`);
   //   console.log(`the parsed date is ${Date.parse(d)}`);
 
-  let daysRemain = Math.ceil((Date.parse(date) - Date.parse(d)) / 86400000); //1000ms*60min*60hour*24day
+  let daysRemain = Math.ceil((Date.parse(date) - Date.parse(d)) / 86400000); //1000ms*60sec*60min*24hours
   //   console.log(`remaining days are ${daysRemain}`);
   return daysRemain <= 0 ? alert("Trip must start from Tomorrow") : daysRemain;
 };
